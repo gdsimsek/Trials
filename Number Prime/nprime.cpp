4bb893375258de8ed737b5099a00aed829027ad7
gdsimsek <132619567+gdsimsek@users.noreply.github.com>
2023-12-04 23:10:32 +0300

    numprime

---
+++ Number Prime/nprime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
